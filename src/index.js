e6d5658b531f31d5452918e7f10dc3027a990e97
Vladimir Bessonov <bessmy@gmail.com>
2021-03-01 10:38:02 +0300

    change entry point in package.json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import app from './app.js';
+import init from './init.js';
 
-app();
+init();
---
+++ src/init.js
@@ -0,0 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import app from './app.js';
+
+export default () => {
+  app();
+};
