01a40c369935c5e094906aaa52c933ca5a9b999c
Vladimir Bessonov <bessmy@gmail.com>
2021-03-02 14:19:39 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -8,90 +8,90 @@ import resources from './locales';
 import getWatcher from './watchers';
 import loader from './loader.js';
 
-const updateInterval = 5000;
+export default () => {
+  const updateInterval = 5000;
 
-const rssState = {
-  lng: 'ru',
-  status: 'ready',
-  form: {
-    validStatus: true,
-    message: '',
-  },
-  feeds: [],
-  posts: [],
-};
+  const rssState = {
+    lng: 'ru',
+    status: 'ready',
+    form: {
+      validStatus: true,
+      message: '',
+    },
+    feeds: [],
+    posts: [],
+  };
 
-const i18next = i18n.createInstance();
-i18next.init({
-  lng: rssState.lng,
-  resources,
-});
-
-const watcher = getWatcher(rssState, pageElements, i18next);
-
-const getParsedData = (data) => {
-  const parser = new DOMParser();
-  return parser.parseFromString(data, 'text/xml');
-};
-
-const updatePosts = (state) => {
-  const { feeds, posts } = state;
-
-  if (state.feeds.length === 0) {
-    return setTimeout(updatePosts, updateInterval, state);
-  }
-
-  const promises = feeds.map((feed) => {
-    const { url, id } = feed;
-    return loader(url)
-      .then((response) => getParsedData(response.data.contents))
-      .then((parsedData) => getUpdatedPosts(parsedData, posts, id))
-      .then((newPosts) => {
-        console.log('!!!%%%!!!!', url);
-        watcher.posts.push(...newPosts);
-      });
+  const i18next = i18n.createInstance();
+  i18next.init({
+    lng: rssState.lng,
+    resources,
   });
 
-  return Promise.all(promises)
-    .then(() => setTimeout(updatePosts, updateInterval, state));
-};
+  const watcher = getWatcher(rssState, pageElements, i18next);
 
-const onSubmitForm = (e) => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  const inputValueUrl = formData.get('url');
-  const addedFeedsUrl = rssState.feeds.map((feed) => feed.url);
+  const getParsedData = (data) => {
+    const parser = new DOMParser();
+    return parser.parseFromString(data, 'text/xml');
+  };
 
-  validateUrl(inputValueUrl, addedFeedsUrl, i18next)
-    .then(() => {
-      watcher.form.validStatus = true;
-      watcher.form.message = i18next.t('messages.loading');
-      watcher.status = 'waiting';
-    })
-    .then(() => {
-      loader(inputValueUrl)
+  const updatePosts = (state) => {
+    const { feeds, posts } = state;
+
+    if (state.feeds.length === 0) {
+      return setTimeout(updatePosts, updateInterval, state);
+    }
+
+    const promises = feeds.map((feed) => {
+      const { url, id } = feed;
+      return loader(url)
         .then((response) => getParsedData(response.data.contents))
-        .then((parsedData) => getNewFeedAndPosts(parsedData, inputValueUrl))
-        .then(({ newFeed, newPosts }) => {
-          console.log('!!!!!!!!!!', inputValueUrl);
-          watcher.form.message = i18next.t('messages.loadedSuccess');
-          watcher.feeds.push(newFeed);
+        .then((parsedData) => getUpdatedPosts(parsedData, posts, id))
+        .then((newPosts) => {
+          console.log('!!!%%%!!!!', url);
           watcher.posts.push(...newPosts);
-          watcher.status = 'ready';
-        })
-        .catch(() => {
-          watcher.form.message = i18next.t('errors.network');
-          watcher.status = 'ready';
         });
-    })
-    .catch((error) => {
-      const errorMessage = error.errors[0];
-      watcher.form.message = errorMessage;
-      watcher.form.validStatus = false;
     });
-};
 
-export default () => {
+    return Promise.all(promises)
+      .then(() => setTimeout(updatePosts, updateInterval, state));
+  };
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const inputValueUrl = formData.get('url');
+    const addedFeedsUrl = rssState.feeds.map((feed) => feed.url);
+
+    validateUrl(inputValueUrl, addedFeedsUrl, i18next)
+      .then(() => {
+        watcher.form.validStatus = true;
+        watcher.form.message = i18next.t('messages.loading');
+        watcher.status = 'waiting';
+      })
+      .then(() => {
+        loader(inputValueUrl)
+          .then((response) => getParsedData(response.data.contents))
+          .then((parsedData) => getNewFeedAndPosts(parsedData, inputValueUrl))
+          .then(({ newFeed, newPosts }) => {
+            console.log('!!!!!!!!!!', inputValueUrl);
+            watcher.form.message = i18next.t('messages.loadedSuccess');
+            watcher.feeds.push(newFeed);
+            watcher.posts.push(...newPosts);
+            watcher.status = 'ready';
+          })
+          .catch(() => {
+            watcher.form.message = i18next.t('errors.network');
+            watcher.status = 'ready';
+          });
+      })
+      .catch((error) => {
+        const errorMessage = error.errors[0];
+        watcher.form.message = errorMessage;
+        watcher.form.validStatus = false;
+      });
+  };
+
   changeLanguage(i18next, pageElements);
 
   updatePosts(rssState, watcher);
