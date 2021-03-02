1fda0533542dfde3e6f24c84fcc5f220c3c6f369
Vladimir Bessonov <bessmy@gmail.com>
2021-03-02 15:53:49 +0300

    fix

--- src/watchers/status.js
+++ src/watchers/status.js
@@ -1,5 +1,12 @@
 export default (value, pageElements) => {
-  const { addButton } = pageElements;
-  if (value === 'waiting') addButton.disabled = true;
-  if (value === 'ready') addButton.removeAttribute('disabled');
+  const { addButton, input } = pageElements;
+  console.log(input)
+  if (value === 'waiting') {
+    addButton.disabled = true;
+    input.readOnly = true;
+  }
+  if (value === 'ready') {
+    addButton.disabled = false;
+    input.readOnly = false;
+  }
 };
